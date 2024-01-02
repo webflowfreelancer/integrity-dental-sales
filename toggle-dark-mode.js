7476f6d70772450762567516d48cd1571a9d94de
Jordan Ryskamp <146259761+webflowfreelancer@users.noreply.github.com>
2024-01-02 21:13:07 +0000

    feat: Added toggleDarkModeButton click event listener
    Added console.log statement and toggle 'dark-mode' class on 'click' event

--- toggle-dark-mode.js
+++
@@ -1,6 +0,0 @@
-document
-  .getElementById("toggleDarkModeButton")
-  .addEventListener("click", function () {
-    console.log("Button clicked!"); // Added console.log statement
-    document.body.classList.toggle("dark-mode");
-  });
